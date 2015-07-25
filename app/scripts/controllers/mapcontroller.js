dccd3edea928788c4ccce7c34bb71b409891a6fc
yglin <yglin.mlanser@gmail.com>
2015-07-25 17:54:28 +0800

    Change clickedMarker icon and add onClick callback

--- app/scripts/controllers/mapcontroller.js
+++ app/scripts/controllers/mapcontroller.js
@@ -11,13 +11,34 @@ angular.module('spirit99App')
 .controller('MapController', ['$scope', 'uiGmapGoogleMapApi', '$mdDialog', 'ygError', 'ygProgress', 'ygServer',
 function($scope, uiGmapGoogleMapApi, $mdDialog, ygError, ygProgress, ygServer) {
 
-    $scope.onClickMap = function(googleMaps, eventName, args){
-        $scope.clickedMarker.coords = {
-            latitude: args[0].latLng.lat(),
-            longitude: args[0].latLng.lng()
-        };
-        $scope.clickedMarker.options.visible = true;
+    $scope.posts = [];
+    $scope.mapIsReady = false;
+    $scope.map = {
+        center:{
+            latitude: 23.973875,
+            longitude: 120.982024
+        },
+        zoom: 6,
+        events: {
+        }
+    };
 
+    $scope.clickedMarker = {
+        id: 'spirit99-map-clicked-marker',
+        coords: {
+            latitude: 23.973875,
+            longitude: 120.982024
+        },
+        // show: false,
+        options: {
+            icon: 'images/icon-question-48.png',
+            visible: false
+        },
+        events: {
+        }
+    };
+
+    $scope.popStoryEditor = function () {
         $mdDialog.show({
             templateUrl: 'views/StoryEditor.html',
             controller: 'StoryEditorController',
@@ -32,6 +53,7 @@ function($scope, uiGmapGoogleMapApi, $mdDialog, ygError, ygProgress, ygServer) {
                     promise.then(function (result) {
                         console.log('Success, post added!!');
                         $scope.reloadPosts();
+                        $scope.clickedMarker.options.visible = false;
                     }, function (error) {
                         console.log('BoooooooM~!!!, adding post failed');
                 }));
@@ -39,33 +61,8 @@ function($scope, uiGmapGoogleMapApi, $mdDialog, ygError, ygProgress, ygServer) {
                 console.log('Not connected to post resources');
             }
         }, function(){
-            console.log('你又按錯啦');
+            console.log('你又按錯啦你');
         });
-    };
-
-    $scope.posts = [];
-    $scope.mapIsReady = false;
-    $scope.map = {
-        center:{
-            latitude: 23.973875,
-            longitude: 120.982024
-        },
-        zoom: 6,
-        events: {
-            click: $scope.onClickMap
-        }
-    };
-    $scope.clickedMarker = {
-        create: false,
-        id: 'spirit99-map-clicked-marker',
-        coords: {
-            latitude: 23.973875,
-            longitude: 120.982024
-        },
-        // show: false,
-        options: {
-            visible: false
-        },
     };
 
     $scope.reloadPosts = function(){
@@ -80,6 +77,19 @@ function($scope, uiGmapGoogleMapApi, $mdDialog, ygError, ygProgress, ygServer) {
             console.log("Post resource is null, maybe not connected to server?");
         }
     }
+
+    $scope.map.events.click = function (googleMaps, eventName, args){
+        $scope.clickedMarker.coords = {
+            latitude: args[0].latLng.lat(),
+            longitude: args[0].latLng.lng()
+        };
+        $scope.clickedMarker.options.visible = true;
+        $scope.popStoryEditor();
+    };
+
+    $scope.clickedMarker.events.click = function (marker, eventName, model) {
+        $scope.popStoryEditor();
+    };
 
     // Center map at user's current location
     $scope.centerGeoLocation = function(map, zoom){
