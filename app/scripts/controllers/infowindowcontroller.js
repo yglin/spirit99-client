c57dedd6ec4dd5957465c19f413eec718d9ebda2
yglin <yglin.mlanser@gmail.com>
2015-08-18 17:55:09 +0800

    Show infoWindow on mouse over markers

--- app/scripts/controllers/infowindowcontroller.js
+++ app/scripts/controllers/infowindowcontroller.js
@@ -8,17 +8,5 @@
  * Controller of the spirit99App
  */
 angular.module('spirit99App')
-.controller('InfoWindowController', ['$scope', '$http', 'ygServer', function ($scope, $http, ygServer) {
-    // console.log($scope.parameter);
-    // $scope.parameter.thumbnail = 'http://www.thefinancialblogger.com/wp-content/uploads/2012/12/dr-evil-150x150.jpg';
-    if(!($scope.parameter.thumbnail) && !(ygServer.currentServer.noThumbnail)){
-        var thumbnailUrl = ygServer.currentServer.postUrl + '/' + $scope.parameter.id + '/thumbnail';
-        $http.get(thumbnailUrl).then(
-            function (response) {
-                $scope.parameter.thumbnail = response;
-            },
-            function (error) {
-                console.log(error);
-        });
-    }
+.controller('InfoWindowController', ['$scope', function ($scope) {
 }]);
