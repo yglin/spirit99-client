7191c589708e16fdb1a5550b7b4abc613d56dafd
yglin <yglin.mlanser@gmail.com>
2015-08-20 11:46:20 +0800

    Open and close side ListPosts

--- app/scripts/controllers/listposts.js
+++ app/scripts/controllers/listposts.js
@@ -8,11 +8,15 @@
  * Controller of the spirit99App
  */
 angular.module('spirit99App')
-.controller('ListPostsController', ['$scope', 'ygUserPref', function ($scope, ygUserPref) {
+.controller('ListPostsController', ['$scope', '$mdSidenav', 'ygUserPref', function ($scope, $mdSidenav, ygUserPref) {
     $scope.lockedOpen = false;
     $scope.$watch(function () {
         return ygUserPref.$storage.openListPosts;
     }, function (newValue, oldValue) {
         $scope.lockedOpen = newValue;
     });
+
+    $scope.close = function () {
+        ygUserPref.$storage.openListPosts = false;
+    };
 }]);
