902b7c0e465f93f5701986c6ba7efafcb10c69ab
yglin <yglin.mlanser@gmail.com>
2015-10-04 20:12:04 +0800

    Add user-settings pane in sidenav menu

---
+++ app/scripts/controllers/user-settings.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spirit99App.controller:UserSettingsCtrl
+ * @description
+ * # UserSettingsCtrl
+ * Controller of the spirit99App
+ */
+angular.module('spirit99App')
+.controller('UserSettingsController', ['$scope', function ($scope) {
+    
+}]);
