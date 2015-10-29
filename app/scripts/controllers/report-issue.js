60a6db36ea7efcc64a1999629c7f2dd38a3ee331
yglin <yglin.mlanser@gmail.com>
2015-10-29 19:54:14 +0800

    Issue Report: fix bug of issue.description

--- app/scripts/controllers/report-issue.js
+++ app/scripts/controllers/report-issue.js
@@ -11,20 +11,22 @@ angular.module('spirit99App')
 .controller('ReportIssueController', ['$scope', '$log', '$http', '$window', '$mdDialog', 'ygFroala', 'ygServer',
 function ($scope, $log, $http, $window, $mdDialog, ygFroala, ygServer) {
 
-    $scope.description = '<p>天殺的這網站是怎樣#@$%!!</p><img src="http://static2.fjcdn.com/thumbnails/comments/5483060+_11224083b0f38dbfdfb203db56bfe1d3.gif" />';
     
     ygFroala.getFroalaOptions().then(function(options){
         $scope.froalaOptions = options;
         $scope.froalaOptions.minHeight = 100;
     });
+    $scope.issue = {
+        description: '<p>天殺的這網站是怎樣#@$%!!</p><img src="http://static2.fjcdn.com/thumbnails/comments/5483060+_11224083b0f38dbfdfb203db56bfe1d3.gif" />'
+    }
 
     // $scope.isSendingLogHistory = false;
-    $scope.logs = $log.getHistory('error').concat($log.getHistory('ajax'));
+    $scope.issue.error_logs = $log.getHistory('error').concat($log.getHistory('ajax'));
     // Sort logs by timestamp
-    $scope.logs.sort(function (log1, log2) {
+    $scope.issue.error_logs.sort(function (log1, log2) {
         return log1.timestamp - log2.timestamp;
     });
-    $scope.readableLogs = $scope.logs.map(function (element) {
+    $scope.readableLogs = $scope.issue.error_logs.map(function (element) {
         return {
             timestamp: element.timestamp.toLocaleString(),
             context: angular.toJson(element.context)
@@ -41,14 +43,13 @@ function ($scope, $log, $http, $window, $mdDialog, ygFroala, ygServer) {
             $window.alert('電台不支援問題回報功能');
         }
         else{
-            var issueData = {
-                description: $scope.description
-            };
-            if($scope.isSendingLogHistory){
-                issueData.error_logs = $scope.logs;
+            // console.log($scope.issue.description);
+            if(!$scope.isSendingLogHistory){
+                $scope.issue.error_logs = [];
             }
-            $http.post(canReportIssue.url, issueData).then(
+            $http.post(canReportIssue.url, $scope.issue).then(
             function (response) {
+                $window.alert('問題已成功回報，感謝您為網站的進步盡一份心力:)');
                 $log.info('Issue reported: ');
                 $log.info(response);
             },
