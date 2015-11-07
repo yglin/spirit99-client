ab84e001899449e5efe21e106d47943e842ea14f
yglin <yglin.mlanser@gmail.com>
2015-11-07 12:02:30 +0800

    Toast tips not interrupt previous one

--- app/scripts/services/yg-toast-tip.js
+++ app/scripts/services/yg-toast-tip.js
@@ -8,14 +8,19 @@
  * Service in the spirit99App.
  */
 angular.module('spirit99App')
-.service('ygToastTip',['$mdToast', 'ygUserPref',
-function ($mdToast, ygUserPref) {
+.service('ygToastTip',['$q', '$mdToast', 'ygUserPref',
+function ($q, $mdToast, ygUserPref) {
     var self = this;
+    self.isShowingToast = false;
     self.showToastTip = function (content) {
-        if(ygUserPref.$storage.showToastTips){
+        if(ygUserPref.$storage.showToastTips && !self.isShowingToast){
+            self.isShowingToast = true;
             $mdToast.show({
                 template: '<md-toast>' + content + '</md-toast>'
-            });            
+            })
+            .finally(function () {
+                self.isShowingToast = false;
+            });
         }
     };
 }]);
